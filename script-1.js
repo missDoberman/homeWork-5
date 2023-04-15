a9d5588e9a3bfc8ff41d6a9166fb46324d8d0edf
Oksana <example@gmail.com>
2023-04-15 10:26:54 +0700

    first

---
+++ script-1.js
@@ -0,0 +1,44 @@
+'use strict';
+// Задача 1
+// Запрограммируйте поведение кнопки по нажатию на нее (она меняет текст в 
+// span)
+// function buttonClick(){
+//     let elem = document.getElementById('elem');
+//     elem.innerHTML = 'Ку-ку! А я <strong>жирный!</strong>';
+
+// }
+
+
+
+// Задача 5
+
+// Повторите страницу по данному по образцу 
+// (при нажатии на кнопку текст в каждом абзаце поменяется на порядковый номер абзаца)
+
+
+
+// function func(){
+//     let elems = document.getElementsByTagName('p');
+//     for(let i = 0; i<elems.length; i++){
+//         elems[i].innerHTML = i; 
+//     }
+// }
+
+
+// Задача 3
+// function buttonClick(){
+//     let elem = document.getElementById('elem');
+//     elem.outerHTML = '<b>' +elem.innerHTML+'</b>';
+// }
+
+
+
+
+// Задача 2
+// Запрограммируйте поведение кнопки по нажатию на нее (она меняет текст в 
+// span)
+// function buttonClick(){
+//     let elem = document.getElementById('elem');
+//     elem.innerHTML = 'Абзац превратился в h3';
+//     elem.outerHTML = '<b>' +elem.innerHTML+'</b>';
+// }
